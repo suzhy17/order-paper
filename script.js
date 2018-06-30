baeb7614b12748e9d5bee16ae8cc254689dd39b5
suzhy17 <suzhy17@gmail.com>
2018-07-01 01:27:36 +0900

    - 악세사리 입력 가능하도록 (코드 읽지 못하는 문제 수정)   => 코일룩,배터리만 적용 완료   => 강화유리, LED 등 나머지는 테스트가 필요함 - 탭 순서 : 블랙->트윙클_레더_소프트_스피릿_디자인커버_코일룩_스마트클리너_보조배터리

--- script.js
+++ script.js
@@ -7,26 +7,48 @@ const thumbnailRoot = 'C:/thumbnail';
 const outputRoot = 'C:/발주서';
 
 const templates = new Map([
-    ['SOFTT'      , {type: 'case', label: '소프트케이스'        , price: 5500}],
-    ['BLACK'      , {type: 'case', label: '블랙케이스'          , price: 7700}],
-    ['TWINK'      , {type: 'case', label: '트윙클케이스'        , price: 8800}],
-    ['LEATH'      , {type: 'case', label: '레더케이스'          , price: 8800}],
-    ['SPRIT'      , {type: 'case', label: '스피릿케이스'        , price: 9350}],
-    ['SPRITSP'    , {type: 'case', label: '스피릿케이스(커버)'  , price: 4400}],
-    ['GLASS'      , {type: 'case', label: '강화유리'            , price: 2200}],
-    ['FGLASS'     , {type: 'case', label: '풀커버'              , price: 6600}],
-    ['FFGLASS'    , {type: 'case', label: '전면부착풀커버'      , price: 13200}],
-    ['BTTRY_5CA'  , {type: 'acce', label: '보조배터리 5,000mAh' , price: 12100}],
-    ['BTTRY_10CA' , {type: 'acce', label: '보조배터리 10,000mAh', price: 18700}],
-    ['ACC_COILL'  , {type: 'acce', label: '코일룩'              , price: 4400}],
-    ['SMART'      , {type: 'acce', label: '스마트클리너'        , price: 1650}],
-    ['IPJEN'      , {type: 'acce', label: '아이폰젠더'          , price: 550}],
-    ['CABLEPK'    , {type: 'acce', label: '애플케이블 핑크'     , price: 3300}],
-    ['CABLESB'    , {type: 'acce', label: '애플케이블 실버'     , price: 3300}],
-    ['FDLED'      , {type: 'acce', label: '접이식LED'           , price: 6600}],
-    ['GOMLED'     , {type: 'acce', label: '곰토끼LED'           , price: 4620}],
-    ['CYLED'      , {type: 'acce', label: '원형LED'             , price: 5500}],
+    ['BLACK'     , {type: 'case', label: '블랙케이스'          , tabGroup: 'BLACK'  , price: 7700}],
+    ['TWINK'     , {type: 'case', label: '트윙클케이스'        , tabGroup: 'TWINK'  , price: 8800}],
+    ['LEATH'     , {type: 'case', label: '레더케이스'          , tabGroup: 'LEATH'  , price: 8800}],
+    ['SOFTT'     , {type: 'case', label: '소프트케이스'        , tabGroup: 'SOFTT'  , price: 5500}],
+    ['SPRIT'     , {type: 'case', label: '스피릿케이스'        , tabGroup: 'SPRIT'  , price: 9350}],
+    ['SPRITSP'   , {type: 'case', label: '스피릿케이스(커버)'  , tabGroup: 'SPRITSP', price: 4400}],
+    ['ACC_COILL' , {type: 'acce', label: '코일룩'              , tabGroup: 'COILL'  , price: 4400}],
+    ['SMART'     , {type: 'acce', label: '스마트클리너'        , tabGroup: 'SMART'  , price: 1650}],
+    ['BTTRY_5CA' , {type: 'acce', label: '보조배터리 5,000mAh' , tabGroup: 'BTTRY'  , price: 12100}],
+    ['BTTRY_10CA', {type: 'acce', label: '보조배터리 10,000mAh', tabGroup: 'BTTRY'  , price: 18700}],
+    ['GLASS'     , {type: 'acce', label: '강화유리'            , tabGroup: 'GLASS'  , price: 2200}],
+    ['FGLASS'    , {type: 'acce', label: '풀커버'              , tabGroup: 'GLASS'  , price: 6600}],
+    ['FFGLASS'   , {type: 'acce', label: '전면부착풀커버'      , tabGroup: 'GLASS'  , price: 13200}],
+    ['IPJEN'     , {type: 'acce', label: '아이폰젠더'          , tabGroup: 'IPJEN'  , price: 550}],
+    ['CABLEPK'   , {type: 'acce', label: '애플케이블 핑크'     , tabGroup: 'CABLE'  , price: 3300}],
+    ['CABLESB'   , {type: 'acce', label: '애플케이블 실버'     , tabGroup: 'CABLE'  , price: 3300}],
+    ['FDLED'     , {type: 'acce', label: '접이식LED'           , tabGroup: 'LED'    , price: 6600}],
+    ['GOMLED'    , {type: 'acce', label: '곰토끼LED'           , tabGroup: 'LED'    , price: 4620}],
+    ['CYLED'     , {type: 'acce', label: '원형LED'             , tabGroup: 'LED'    , price: 5500}],
 ]);
+
+const tabGroup = new Map([
+    ['BLACK'  , {label: '블랙케이스'    , sort: 1}],
+    ['TWINK'  , {label: '트윙클케이스'  , sort: 2}],
+    ['LEATH'  , {label: '레더케이스'    , sort: 3}],
+    ['SOFTT'  , {label: '소프트케이스'  , sort: 4}],
+    ['SPRIT'  , {label: '스피릿케이스'  , sort: 5}],
+    ['SPRITSP', {label: '디자인커버'    , sort: 6}],
+    ['COILL'  , {label: '코일룩'        , sort: 7}],
+    ['SMART'  , {label: '스마트클리너'  , sort: 8}],
+    ['BTTRY'  , {label: '보조배터리'    , sort: 9}],
+    ['GLASS'  , {label: '강화유리'      , sort: 10}],
+    ['IPJEN'  , {label: '아이폰젠더'    , sort: 11}],
+    ['CABLE'  , {label: '애플케이블'    , sort: 12}],
+    ['LED'    , {label: 'LED'           , sort: 13}],
+]);
+
+const exceptionTemplates = [
+    'BTTRY_5CA',
+    'BTTRY_10CA',
+    'ACC_COILL'
+];
 
 const devices = new Map([
     ['IP5', '아이폰5'],
@@ -52,6 +74,9 @@ const devices = new Map([
     ['OG5', '옵티머스G5'],
     ['OG6', '옵티머스G6'],
     ['OG7', '옵티머스G7'],
+    ['5CA', '5,000mAh'],
+    ['10CA', '10,000mAh'],
+    ['OG7', '옵티머스G7'],
     ['', '단일'],
 ]);
 
@@ -69,7 +94,7 @@ let orderDate = '';
 $(document).ready(function () {
 
     $('#create').click(() => {
-        try {
+        //try {
 
             if (!gridData) {
                 alert('주문 파일을 먼저 선택하세요.');
@@ -87,22 +112,20 @@ $(document).ready(function () {
                 // alert('orderDate=' + orderDate);
             } catch (e) {
                 alert(`주문 파일명을 확인해주세요.\n파일명 마지막에 '_년월일'이 있어야합니다.\n예> 매장발주서_20180627.xlsx`);
+                return;
             }
 
             // 입력 데이터 파싱, 검증
             let inputDatas = [];
             for (let i = 0; i < gridData.length; i++) {
-                let isCoill = false;
-                if (gridData[i].EPS_DESIGN_CODE.includes('ACC_COILL')) {
-                    isCoill = true;
-                }
-                let templateCode = gridData[i].TEMPLATE_CODE.split('_');
-                let epsDesignCode = gridData[i].EPS_DESIGN_CODE.split('_');
+                let designTemplateDevice = parseEpsDesignCode(gridData[i].EPS_DESIGN_CODE);
+
                 inputDatas[i] = {
                     shop: gridData[i].REQUESTER,
-                    template: isCoill ? 'ACC_COILL' : templateCode[0],
-                    device: isCoill ? '' : templateCode[1],
-                    designCode: epsDesignCode[0],
+                    tabGroup: designTemplateDevice.tabGroup,
+                    template: designTemplateDevice.template,
+                    device: designTemplateDevice.device,
+                    designCode: designTemplateDevice.design,
                     designSubCode: gridData[i].DESIGN_SUB_CODE,
                     quantity: parseInt(gridData[i].QUANTITY)
                 };
@@ -118,6 +141,7 @@ $(document).ready(function () {
                 if (!res[resKey]) {
                     res[resKey] = {
                         shop: value.shop,
+                        tabGroup: value.tabGroup,
                         template: value.template,
                         device: value.device,
                         designCode: value.designCode,
@@ -130,6 +154,10 @@ $(document).ready(function () {
                 return res;
             }, {});
 
+            console.log('==================');
+            rawData.forEach((data) => {
+                console.log(`shop: ${data.shop}, tabGroup: ${data.tabGroup}, template: ${data.template}, device: ${data.device}, designCode: ${data.designCode}, designSubCode: ${data.designSubCode}, quantity: ${data.quantity}`);
+            });
 
             // 정렬
             rawData = rawData.sort((a, b) => {
@@ -183,13 +211,42 @@ $(document).ready(function () {
             }
 
             alert(`[${outputRoot}] 폴더에 ${shopCnt} 개의 발주서가 생성되었습니다.`);
-        }
-        catch (e) {
-            console.log(`[${e.name}] ${e.message}`);
-            alert(e.message);
-        }
+        // }
+        // catch (e) {
+        //     console.log(`[${e.lineNumber}][${e.name}] ${e.message}`);
+        //     alert(e.message);
+        // }
     });
 });
+
+var parseEpsDesignCode = function (epsDesignCode) {
+    let exceptionTemplate = '';
+    exceptionTemplates.forEach((templateName) => {
+        if (epsDesignCode.includes(templateName)) {
+            exceptionTemplate = templateName;
+        }
+    });
+
+    let arrEpsDesignCode = epsDesignCode.split('_');
+
+    if (exceptionTemplate) {
+        return {
+            design: arrEpsDesignCode[0],
+            template: exceptionTemplate,
+            device: arrEpsDesignCode[2],
+            tabGroup: templates.get(exceptionTemplate).tabGroup
+        };
+    } else {
+        return {
+            design: arrEpsDesignCode[0],
+            template: arrEpsDesignCode[1],
+            device: arrEpsDesignCode[2],
+            tabGroup: templates.get(arrEpsDesignCode[1]).tabGroup
+        };
+    }
+
+    return false;
+};
 
 /**
  * 존재하지 않는 기기코드가 있는지 체크
@@ -205,7 +262,7 @@ var checkDevice = function (inputDatas) {
     if (missingDevices.length > 0) {
         throw (new Error(`일부 기기 코드[${missingDevices.join(", ")}]가 존재하지 않습니다. 설정파일에 기기정보를 먼저 등록하고 프로그램을 재시작 하세요.`));
     }
-}
+};
 
 /**
  * 발주서 생성
@@ -223,13 +280,14 @@ var createOrderPaper = function (rawData) {
             totalPrice: 0
         };
         for (let i = 0; i < rawData.length; i++) {
-            let platform = templates.get(rawData[i].template);
-            if (platform.type === 'case') {
+            let tmpl = templates.get(rawData[i].template);
+            if (tmpl.type === 'case') {
                 total.caseQty += parseInt(rawData[i].quantity);
             } else {
                 total.acceQty += parseInt(rawData[i].quantity);
             }
-            total.totalPrice += platform.price * rawData[i].quantity;
+            total.totalPrice += tmpl.price * rawData[i].quantity;
+            console.log(`template=${tmpl.label}, ${tmpl.price} * ${rawData[i].quantity} = ${total.totalPrice}`);
         }
 
         $('#totalCaseQuantity').text(total.caseQty.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
@@ -242,7 +300,6 @@ var createOrderPaper = function (rawData) {
         setOrderPaperOrderRow(orderPaperDataMap);
 
         // 상세 주문내역용 데이터 구조화
-        //let orderDetailDataMap = createOrderDetailData(rawData);
         let orderDetailDataMap = createThumbnailData(rawData);
         setOrderDetailTemplateTab(orderDetailDataMap);
 
@@ -254,7 +311,7 @@ var createOrderPaper = function (rawData) {
             console.log('write ok');
         });
     });
-}
+};
 
 
 /**
@@ -264,17 +321,17 @@ var createOrderPaper = function (rawData) {
  */
 let createThumbnailData = (rawData) => {
     console.log('=== createThumbnailData ===');
-    let templateMap = new Map();
+    let tabGroupMap = new Map();
     for (let i = 0; i < rawData.length; i++) {
-        if (templateMap.has(rawData[i].template)) {
+        if (tabGroupMap.has(rawData[i].tabGroup)) {
             continue;
         }
-        templateMap.set(rawData[i].template, []);
+        tabGroupMap.set(rawData[i].tabGroup, []);
     }
 
-    templateMap.forEach((value, key, map) => {
+    tabGroupMap.forEach((value, key, map) => {
         // 현재 key에 해당하는 템플릿만 추출
-        let filteredList = rawData.filter((data) => data.template === key);
+        let filteredList = rawData.filter((data) => data.tabGroup === key);
         let groupByDevice = groupBy(filteredList, 'device');
         console.log(`groupByDevice=${JSON.stringify(groupByDevice)}`);
 
@@ -285,32 +342,37 @@ let createThumbnailData = (rawData) => {
             // groupByDevice[deviceKey].
         }
 
-        templateMap.set(key, groupByDevice);
+        tabGroupMap.set(key, groupByDevice);
+    });
+    tabGroupMap.forEach((value, key, map) => {
+        console.log(`tabGroupMap.key=${key}`);
     });
 
-    return templateMap;
+    return tabGroupMap;
 };
 
 /**
- * 템플릿별로 그룹핑하여 주문 목록 생성
+ * 탭그룹별로 그룹핑하여 주문 목록 생성
  * @param rawData
  * @returns {Map}
  */
 var createOrderPaperData = (rawData) => {
-    let templateMap = new Map();
+
+    let tabGroupMap = new Map();
     for (let i = 0; i < rawData.length; i++) {
-        if (templateMap.has(rawData[i].template)) {
+        if (tabGroupMap.has(rawData[i].tabGroup)) {
             continue;
         }
-        templateMap.set(rawData[i].template, []);
+        tabGroupMap.set(rawData[i].tabGroup, []);
     }
 
-    templateMap.forEach((value, key, map) => {
+
+    tabGroupMap.forEach((value, key, map) => {
         console.log(`key=${key}`);
 
-        // 현재 key에 해당하는 템플릿만 추출
+        // 현재 key에 해당하는 탭그룹만 추출
         let filteredRawData = rawData.filter((data) => {
-            return data.template === key;
+            return data.tabGroup === key;
         });
 
         // 주문 목록 생성
@@ -330,6 +392,7 @@ var createOrderPaperData = (rawData) => {
             // 없는것이면 신규 생성
             if (!isExist) {
                 orderPaperData.push({
+                    tabGroup: data.tabGroup,
                     template: data.template,
                     device: data.device,
                     quantity: data.quantity
@@ -337,10 +400,10 @@ var createOrderPaperData = (rawData) => {
             }
         }
         console.log(`orderPaperData.length=${orderPaperData.length}`);
-        templateMap.set(key, orderPaperData);
+        tabGroupMap.set(key, orderPaperData);
     });
 
-    return templateMap;
+    return tabGroupMap;
 };
 
 /**
@@ -356,9 +419,9 @@ var setOrderPaperOrderRow = (orderPaperDataMap) => {
         let v = orderPaperDataMap.get(key)[0];
         let firstRow = `
           <tr>
-            <td rowspan="${value.length}"><b>${templates.get(v.template).label}</b></td>
-            <td>${v.template}-${v.device}</td>
-            <td><a href="#${v.template.toLowerCase()}-${v.device.toLowerCase()}">${devices.get(v.device)}</a></td>
+            <td rowspan="${value.length}"><b>${tabGroup.get(v.tabGroup).label}</b></td>
+            <td>${v.template}${v.device ? '-' : ''}${v.device}</td>
+            <td><a href="#${v.template.toLowerCase()}-${v.device.toLowerCase()}">${devices.get(v.device) || '단일'}</a></td>
             <td class="text-right">${v.quantity}</td>
             <td></td>
           </tr>`;
@@ -368,8 +431,8 @@ var setOrderPaperOrderRow = (orderPaperDataMap) => {
             v = orderPaperDataMap.get(key)[i];
             let otherRow = `
               <tr>
-                <td>${v.template}-${v.device}</td>
-                <td><a href="#${v.template.toLowerCase()}-${v.device.toLowerCase()}">${devices.get(v.device)}</a></td>
+                <td>${v.template}${v.device ? '-' : ''}${v.device}</td>
+                <td><a href="#${v.template.toLowerCase()}-${v.device.toLowerCase()}">${devices.get(v.device) || '단일'}</a></td>
                 <td class="text-right">${v.quantity}</td>
                 <td></td>
               </tr>`;
@@ -387,8 +450,25 @@ var setOrderPaperOrderRow = (orderPaperDataMap) => {
 let setOrderDetailTemplateTab = (orderDetailDataMap) => {
     // 좌측 탭메뉴에 템플릿 추가
     let $leftMenu = $('#left-menu');
+    let tabKeys = [];
     orderDetailDataMap.forEach((value, key, map) => {
-        let menuItem = `<li><a href="#tabs-${key.toLowerCase()}">${templates.get(key).label.replace("케이스", "")}</a></li>`;
+        tabKeys.push(key);
+    });
+
+
+    // 정렬
+    tabKeys = tabKeys.sort((a, b) => {
+        if (tabGroup.get(a).sort === tabGroup.get(b).sort) {
+            return 0;
+        } else if (tabGroup.get(a).sort < tabGroup.get(b).sort) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+
+    tabKeys.forEach((key) => {
+        let menuItem = `<li><a href="#tabs-${key.toLowerCase()}">${tabGroup.get(key).label}</a></li>`;
         console.log(`menuItem=${menuItem}`);
         $leftMenu.append(menuItem);
     });
@@ -416,7 +496,7 @@ let setOrderDetailTemplateTab = (orderDetailDataMap) => {
                 </colgroup>
                 <thead>
                   <tr>
-                    <th colspan="10" class="text-red">${devices.get(deviceKey)} ${templates.get(templateKey).label}</th>
+                    <th colspan="10" class="text-red">${devices.get(deviceKey) || '' } ${tabGroup.get(templateKey).label}</th>
                   </tr>
                 </thead>
                 <tbody>`;
